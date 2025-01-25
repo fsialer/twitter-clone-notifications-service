6171a53c9f3ffb7fc5dcb129719939af025b57af
Fernando <asialer05@hotmail.com>
2025-01-25 11:28:01 -0500

    proyecto base

---
+++ src/main/java/com/fernando/ms/notifications/app/twitter_clone_notifications_service/TwitterCloneNotificationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.notifications.app.twitter_clone_notifications_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCloneNotificationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterCloneNotificationsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando/ms/notifications/app/twitter_clone_notifications_service/TwitterCloneNotificationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.notifications.app.twitter_clone_notifications_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneNotificationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
